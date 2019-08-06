01089090daf74206f5efe139fc9093b87258e32a
j48 <j48@users.noreply.github.com>
2019-08-06 03:01:13 -0500

    Update BitcoinTxDoracle.sol

--- apps/bitcoin-tx-doracle/smart-contract/contracts/BitcoinTxDoracle.sol
+++ apps/bitcoin-tx-doracle/smart-contract/contracts/BitcoinTxDoracle.sol
@@ -10,59 +10,53 @@ contract BitcoinTxDoracle is Ownable, IexecDoracle{
 	{
 		bytes32 oracleCallID;
 		uint256 amount;
-		uint256 time;
+		uint256 timestamp;
 	}
 
-	mapping(bytes32 => Tx) public txHash;
+	mapping(bytes32 => Tx) public txAmount;
 	
 	event TxUpdated(
 		bytes32 indexed txHash,
 		bytes32 indexed oracleCallID,
 		uint256 amount,
-		uint256 time);
+		uint256 timestamp);
 
 	// Use _iexecHubAddr to force use of custom iexechub, leave 0x0 for autodetect
 	constructor(address _iexecHubAddr)
 	public IexecDoracle(_iexecHubAddr)
 	{}
 
-	function updateEnv(
-	  address _authorizedApp, 
-	  address _authorizedDataset, 
-	  address _authorizedWorkerpool, 
-	  bytes32 _requiredtag, 
-	  uint256 _requiredtrust)
+	function updateEnv(address _authorizedApp, address _authorizedDataset, address _authorizedWorkerpool, bytes32 _requiredtag, uint256 _requiredtrust)
 	public onlyOwner
 	{
 		_iexecDoracleUpdateSettings(_authorizedApp, _authorizedDataset, _authorizedWorkerpool, _requiredtag, _requiredtrust);
 	}
 
 	function decodeResults(bytes memory results)
-	public pure returns(bytes32, uint256)
-	{ return abi.decode(results, (bytes32, uint256)); }
+	public pure returns(bytes32, bytes32)
+	{ return abi.decode(results, (bytes32, bytes32)); }
 
 	function processResult(bytes32 _oracleCallID)
 	public
 	{
 		bytes32       id;
-		uint256       packedData;
+		bytes32       packedData;
 
 		// Parse results
 		(id, packedData) = decodeResults(_iexecDoracleGetVerifiedResult(_oracleCallID));
 
-		// unpack data
-		uint256   time = uint256(uint40(packedData>>0));
-		uint256   data = uint256(uint216(packedData>>40));
-
 		// Process results
-		// always require newer time 
-		require(txHash[id].time < time, "tx-exists");
-		// there should only ever be 1 entry per tx
-		emit TxUpdated(id, _oracleCallID, data, time);
+		// unpack require data
+		uint256   timestamp = uint256(uint40(uint256(packedData>>0)));
+
+		require(txAmount[id].timestamp < timestamp, "tx-exists");
 		
-		txHash[id].oracleCallID = _oracleCallID;
-		txHash[id].amount       = data;
-		txHash[id].time         = time;
+		// there should only ever be 1 entry per tx
+		emit TxUpdated(id, _oracleCallID, amount, timestamp);
+		
+		txAmount[id].oracleCallID = _oracleCallID;
+		txAmount[id].amount       = uint256(uint40(uint256(packedData>>40)));
+		txAmount[id].timestamp    = timestamp;
 	}
 
 	}
